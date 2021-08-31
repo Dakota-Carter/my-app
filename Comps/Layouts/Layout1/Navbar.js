652317a2cbf6ae9585618b402acd530393da2aa9
Dakota Carter <dakotamd.carter.macbain@gmail.com>
2021-08-31 07:45:39 -0400

    NavBar site logo inserted (flex needs work)

--- Comps/Layouts/Layout1/Navbar.js
+++ Comps/Layouts/Layout1/Navbar.js
@@ -1,8 +1,21 @@
+import Image from "next/image"
+import Link from "next/link"
 import styles from "../../../styles/Layout1.module.css"
 
 const Navbar = (props) => {
   return (
     <nav className={styles.navbar}>
+      <div className={styles.navsitelogo}>
+        <Link href="/">
+          <Image
+            src="/navIcons/test-logo.png"
+            alt="Some SEO description (for blind/read-aloud people)"
+            layout="fill"
+            objectFit="scale-down"
+          />
+        </Link>
+      </div>
+
       <ul className={styles.navbar_nav}>{props.children}</ul>
     </nav>
   )
