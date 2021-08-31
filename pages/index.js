af52986c5060b11900d7c98355d7f5011cc48702
Dakota Carter <dakotamd.carter.macbain@gmail.com>
2021-08-31 09:26:35 -0400

    Dynamic Head for user pages.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Footer1 from "../Comps/Layouts/Footers/Footer1"
 import Layout1 from "../Comps/Layouts/Layout1/Layout"
 import SSG from "../Comps/Render_Notify/SSG"
@@ -5,6 +6,12 @@ import SSG from "../Comps/Render_Notify/SSG"
 const HomePage = () => {
   return (
     <>
+      <Head>
+        <title>Homepage | Official</title>
+        <meta name="keywords" content="SEO_stuff_here json fetch users" />
+        <meta name="description" content="Description for search engines." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <div></div>
       <div>
         <p>Hello World</p>
--- pages/json_fetching/[dynamic_user_pages].js
+++ pages/json_fetching/[dynamic_user_pages].js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Footer1 from "../../Comps/Layouts/Footers/Footer1"
 import Layout1 from "../../Comps/Layouts/Layout1/Layout"
 import Layout2 from "../../Comps/Layouts/Layout2/Layout"
@@ -6,6 +7,12 @@ import SSG from "../../Comps/Render_Notify/SSG"
 const UserDetails = ({ dakk }) => {
   return (
     <>
+      <Head>
+        <title>User | {dakk.name}</title>
+        <meta name="keywords" content="SEO_stuff_here json fetch users" />
+        <meta name="description" content="Description for search engines." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <div>
         <h1>{dakk.name}</h1>
         <p>{dakk.username}</p>
